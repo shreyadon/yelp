8efb6e5c4ba653b09039b1a9ea3a2080c10511ea
Raghu Betina <raghu@firstdraft.com>
2016-12-22 11:04:19 +0000

    Whitelist columns in RestaurantTag for ActiveAdmin

--- app/admin/restaurant_tag.rb
+++ app/admin/restaurant_tag.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RestaurantTag do
 
+ permit_params :restaurant_id, :tag_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
